0cd9b1e30f182380057a164fe831fc394dc29aa9
Bruno Marthe <brunomarthe@hotmail.com>
2022-05-17 21:15:31 -0300

    Estilos de menu e header

--- src/app/modules/als-menu/als-menu.component.ts
+++ src/app/modules/als-menu/als-menu.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlsMenuComponent implements OnInit {
 
+  isOpenMenu: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+    var bodyStyles = document.body.style;
+    bodyStyles.setProperty('--menuSize', "85px");
+    bodyStyles.setProperty('--alignItens', "center");
+    bodyStyles.setProperty('--paddingMenuItens', "0");
+    bodyStyles.setProperty('--headerWith', "90vw");
+  }
+
+  openMenu(){
+    this.isOpenMenu = !this.isOpenMenu
+    var bodyStyles = document.body.style;
+    bodyStyles.setProperty('--menuSize', this.isOpenMenu ? "406px" : "85px");
+    bodyStyles.setProperty('--alignItens', this.isOpenMenu ? "flex-start" : "center");
+    bodyStyles.setProperty('--paddingMenuItens', this.isOpenMenu ? "0 0 0 29px" : "0");
+    bodyStyles.setProperty('--headerWith', this.isOpenMenu ? "67vw" : "90vw");
   }
 
 }
