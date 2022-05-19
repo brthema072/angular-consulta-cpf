bd5aa7db52603d0fb6eba45f4e8f8666ce37df9d
Bruno Marthe <brunomarthe@hotmail.com>
2022-05-19 20:16:29 -0300

    Estilos de label, input, removendo componente button-select

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonSimpleComponent } from './shared/components/button-simple/button-simple.component';
-import { ButtonSelectComponent } from './shared/components/button-select/button-select.component';
 import { AlsHeaderComponent } from './modules/als-header/als-header.component';
 import { AlsMenuComponent } from './modules/als-menu/als-menu.component';
 import { AlsValidateCpfComponent } from './modules/als-validate-cpf/als-validate-cpf.component';
@@ -15,7 +14,6 @@ import { NgxMaskModule } from 'ngx-mask';
     AppComponent,
     AlsHeaderComponent,
     ButtonSimpleComponent,
-    ButtonSelectComponent,
     AlsMenuComponent,
     AlsValidateCpfComponent
   ],
