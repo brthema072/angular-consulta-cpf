10b5cee55883ab605b3d4b4e216a652d0243d8e7
Bruno Marthe <brunomarthe@hotmail.com>
2022-05-20 23:40:09 -0300

    Component show-information

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ShowInformationComponent } from './modules/show-information/show-information.component';
-import { ValidateCpfComponent } from './modules/validate-cpf/validate-cpf.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo:'documentation-information'
+    redirectTo:'validate-cpf'
   },
   {
     path: 'validate-cpf',
-    component: ValidateCpfComponent
-  },
-  {
-    path: 'show-information',
     component: ShowInformationComponent
   },
   {
     path: '**',
-    component: ValidateCpfComponent
+    component: ShowInformationComponent
   }
 ];
 
